d6ba11f363e29a0ed1d33d19e594e3a1ec23c962
Rayyan Pistawala <rayyan_pistawala@wamatechnology.in>
2025-12-26 11:08:56 +0530

    test chagnes

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* mirror test comment*/}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
